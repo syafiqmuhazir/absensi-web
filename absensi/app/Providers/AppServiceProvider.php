ed16b206ce744c0049292a428bbd3c157bf8608e
Syafiq Muhazir <sapikm06@gmail.com>
2025-11-23 08:04:17 +0700

    Render Deploy

--- absensi/app/Providers/AppServiceProvider.php
+++ absensi/app/Providers/AppServiceProvider.php
@@ -21,8 +21,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
-        //
+    public function boot(): void
+{
+    if($this->app->environment('production')) {
+        \URL::forceScheme('https');
     }
+}
 }
